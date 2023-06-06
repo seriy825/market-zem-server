5df6ad2fe9cdbd09d588c843e78561a279fa9784
Serhii Olar <olar_ss@groupbwt.com>
2023-06-06 17:17:50 +0300

    feat:adding listings to favorites by user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\User\UserUpdateRequest;
 use App\Interfaces\UserRepositoryInterface;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Favorites\AddToFavoritesRequest;
+use App\Http\Requests\Favorites\RemoveFromFavoritesRequest;
 
 class UserController extends Controller
 {
@@ -26,6 +28,18 @@ class UserController extends Controller
     }
     public function getListingsByUser($id){
         $response = $this->userRepository->getListingsByUser($id);
+        return response()->json($response,200);
+    }
+    public function addToFavorites($listingId,AddToFavoritesRequest $addToFavoritesRequest){
+        $response = $this->userRepository->addToFavorites($listingId,$addToFavoritesRequest);
+        return response()->json($response,200);
+    }
+    public function removeFromFavorites($listingId,RemoveFromFavoritesRequest $removeFromFavoritesRequest){
+        $response = $this->userRepository->removeFromFavorites($listingId,$removeFromFavoritesRequest);
+        return response()->json($response,200);
+    }
+    public function getFavoritedListings($id){
+        $response = $this->userRepository->getFavoritedListings($id);
         return response()->json($response,200);
     }
 }
