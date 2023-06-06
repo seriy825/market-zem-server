5190be0be9133d707b21dc215f2a63cd471e2820
Serhii Olar <olar_ss@groupbwt.com>
2023-06-06 17:17:50 +0300

    feat:adding listings to favorites by user

--- app/Interfaces/UserRepositoryInterface.php
+++ app/Interfaces/UserRepositoryInterface.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Interfaces;
+
+use App\Http\Requests\Favorites\AddToFavoritesRequest;
+use App\Http\Requests\Favorites\RemoveFromFavoritesRequest;
 use App\Http\Requests\User\UserUpdateRequest;
 use Illuminate\Http\Request;
 
@@ -9,4 +12,7 @@ interface UserRepositoryInterface
     public function getUser($token);
     public function updateUser($id,UserUpdateRequest $user);
     public function getListingsByUser($id);
+    public function getFavoritedListings($id);
+    public function addToFavorites($listingId,AddToFavoritesRequest $addToFavoritesRequest);
+    public function removeFromFavorites($listingId,RemoveFromFavoritesRequest $removeFromFavoritesRequest);
 }
